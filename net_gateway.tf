0b4a29d5f567a83d2b8d4acec92edc7db1bca814
Claudeilton Dantas <fcd007@hotmail.com>
2026-07-29 20:56:34 -0300

    feat(adicionado infra subnet e nat): ajuste de infra network

--- net_gateway.tf
+++ net_gateway.tf
@@ -2,7 +2,7 @@ resource "aws_eip" "vpc_eip_1a" {
   domain = "vpc"
 
   tags = {
-    Name = format("%s-eip", var.project_name)
+    Name = format("%s-eip-1a", var.project_name)
   }
 }
 
@@ -11,6 +11,40 @@ resource "aws_nat_gateway" "nat_1a" {
   subnet_id     = aws_subnet.public_subnet_1a.id
 
   tags = {
-    Name = format("%s-natgw", var.project_name)
+    Name = format("%s-nat-1a", var.project_name)
+  }
+}
+
+resource "aws_eip" "vpc_eip_1b" {
+  domain = "vpc"
+
+  tags = {
+    Name = format("%s-eip-1b", var.project_name)
+  }
+}
+
+resource "aws_nat_gateway" "nat_1b" {
+  allocation_id = aws_eip.vpc_eip_1b.id
+  subnet_id     = aws_subnet.public_subnet_1b.id
+
+  tags = {
+    Name = format("%s-nat-1b", var.project_name)
+  }
+}
+
+resource "aws_eip" "vpc_eip_1c" {
+  domain = "vpc"
+
+  tags = {
+    Name = format("%s-eip-1c", var.project_name)
+  }
+}
+
+resource "aws_nat_gateway" "nat_1c" {
+  allocation_id = aws_eip.vpc_eip_1c.id
+  subnet_id     = aws_subnet.public_subnet_1c.id
+
+  tags = {
+    Name = format("%s-nat-1c", var.project_name)
   }
 }
